95cb80fec46eceece683696f338741b5f7a13de4
Aakash Gill <aakashgill@Precious.local>
2018-01-19 02:48:49 +0530

    Basic gulp config

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify'); // minify js
+var gutil = require('gulp-util'); // log ES6 error
+var sass = require('gulp-ruby-sass'); //sass compiler only
+var prefixer = require('gulp-autoprefixer'); //browser autoprefixer
+var cleanCSS = require('gulp-clean-css');
+ 
+// uglify / minimize javascript
+gulp.task('uglify', function() {
+	gulp.src('app/js/*.js')
+	.pipe(uglify().on('error', function(err) {
+		gutil.log(gutil.colors.red('[Error]'), err.toString());
+		this.emit('end');
+		}))
+	.pipe(gulp.dest('dist/js/'));
+});
+	
+// all the scss files in styles folder
+gulp.task('sass', () =>
+	sass('app/styles/**/*.scss')
+    	.on('error', sass.logError)
+    	.pipe(prefixer('last 3 versions'))
+    	.pipe(cleanCSS())
+    	.pipe(gulp.dest('dist/styles/'))
+);
+
+// watch for changes
+gulp.task('watch', function() {
+    gulp.watch('app/styles/**/*.scss', ['sass']);
+    gulp.watch('app/js/*.js', ['uglify']);
+});
+
+gulp.task('default', ['sass', 'uglify', 'watch']);
